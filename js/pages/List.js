03ddd2270e0e7b891ba04785450651fa2a597ff1
c_asual_mf! <j3510498@gmail.com>
2026-08-24 23:26:54 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -138,7 +138,7 @@ export default {
     }),
     computed: {
         level() {
-            return this.list[this.selected][0];
+            const level = targetObject?.[0] ?? "Default Level";
         },
         video() {
             if (!this.level.showcase) {
