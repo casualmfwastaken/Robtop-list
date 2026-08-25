32e6b5e85a52e249527e9c8ce73d2fc6790f5cf5
c_asual_mf! <j3510498@gmail.com>
2026-08-25 14:45:45 +0100

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -13,7 +13,7 @@ export default {
         <main v-else class="page-roulette">
             <div class="sidebar">
                 <p class="type-label-md" style="color: #aaa">
-                    Shameless copy of the Extreme Demon Roulette by <a href="https://matcool.github.io/extreme-demon-roulette/" target="_blank">matcool</a>.
+                    Shameless copy of the Extreme Demon Roulette by <a href="https://github.io" target="_blank">matcool</a>.
                 </p>
                 <form class="options">
                     <div class="check">
@@ -40,7 +40,6 @@ export default {
             <section class="levels-container">
                 <div class="levels">
                     <template v-if="levels.length > 0">
-                        <!-- Completed Levels -->
                         <div class="level" v-for="(level, i) in levels.slice(0, progression.length)">
                             <a :href="level.video" class="video">
                                 <img :src="getThumbnailFromId(getYoutubeIdFromUrl(level.video))" alt="">
@@ -51,7 +50,6 @@ export default {
                                 <p style="color: #00b54b; font-weight: 700">{{ progression[i] }}%</p>
                             </div>
                         </div>
-                        <!-- Current Level -->
                         <div class="level" v-if="!hasCompleted">
                             <a :href="currentLevel.video" target="_blank" class="video">
                                 <img :src="getThumbnailFromId(getYoutubeIdFromUrl(currentLevel.video))" alt="">
@@ -67,14 +65,12 @@ export default {
                                 <Btn @click.native.prevent="onGiveUp" style="background-color: #e91e63;">Give Up</Btn>
                             </form>
                         </div>
-                        <!-- Results -->
                         <div v-if="givenUp || hasCompleted" class="results">
                             <h1>Results</h1>
                             <p>Number of levels: {{ progression.length }}</p>
                             <p>Highest percent: {{ currentPercentage }}%</p>
                             <Btn v-if="currentPercentage < 99 && !hasCompleted" @click.native.prevent="showRemaining = true">Show remaining levels</Btn>
                         </div>
-                        <!-- Remaining Levels -->
                         <template v-if="givenUp && showRemaining">
                             <div class="level" v-for="(level, i) in levels.slice(progression.length + 1, levels.length - currentPercentage + progression.length)">
                                 <a :href="level.video" target="_blank" class="video">
@@ -102,7 +98,7 @@ export default {
     data: () => ({
         loading: false,
         levels: [],
-        progression: [], // list of percentages completed
+        progression: [],
         percentage: undefined,
         givenUp: false,
         showRemaining: false,
@@ -112,14 +108,12 @@ export default {
         fileInput: undefined,
     }),
     mounted() {
-        // Create File Input
         this.fileInput = document.createElement('input');
         this.fileInput.type = 'file';
         this.fileInput.multiple = false;
         this.fileInput.accept = '.json';
         this.fileInput.addEventListener('change', this.onImportUpload);
 
-        // Load progress from local storage
         const roulette = JSON.parse(localStorage.getItem('roulette'));
 
         if (!roulette) {
@@ -171,7 +165,7 @@ export default {
 
             const fullList = await fetchList();
 
-            if (fullList.filter(([_, err]) => err).length > 0) {
+            if (!fullList || !Array.isArray(fullList) || fullList.filter(([_, err]) => err).length > 0) {
                 this.loading = false;
                 this.showToast(
                     'List is currently broken. Wait until it\'s fixed to start a roulette.',
@@ -187,110 +181,15 @@ export default {
             }));
             const list = [];
             if (this.useMainList) list.push(...fullListMapped.slice(0, 75));
-            if (this.useExtendedList) {
-                list.push(...fullListMapped.slice(75, 150));
-            }
+            if (this.useExtendedList) list.push(...fullListMapped.slice(75));
 
-            // random 100 levels
-            this.levels = shuffle(list).slice(0, 100);
-            this.showRemaining = false;
-            this.givenUp = false;
+            this.levels = this.shuffle(list).slice(0, 100);
             this.progression = [];
+            this.givenUp = false;
+            this.showRemaining = false;
             this.percentage = undefined;
-
-            this.loading = false;
-        },
-        save() {
-            localStorage.setItem(
-                'roulette',
-                JSON.stringify({
-                    levels: this.levels,
-                    progression: this.progression,
-                }),
-            );
-        },
-        onDone() {
-            if (!this.percentage) {
-                return;
-            }
-
-            if (
-                this.percentage <= this.currentPercentage ||
-                this.percentage > 100
-            ) {
-                this.showToast('Invalid percentage.');
-                return;
-            }
-
-            this.progression.push(this.percentage);
-            this.percentage = undefined;
-
             this.save();
-        },
-        onGiveUp() {
-            this.givenUp = true;
-
-            // Save progress
-            localStorage.removeItem('roulette');
-        },
-        onImport() {
-            if (
-                this.isActive &&
-                !window.confirm('This will overwrite the currently running roulette. Continue?')
-            ) {
-                return;
-            }
-
-            this.fileInput.showPicker();
-        },
-        async onImportUpload() {
-            if (this.fileInput.files.length === 0) return;
-
-            const file = this.fileInput.files[0];
-
-            if (file.type !== 'application/json') {
-                this.showToast('Invalid file.');
-                return;
-            }
-
-            try {
-                const roulette = JSON.parse(await file.text());
-
-                if (!roulette.levels || !roulette.progression) {
-                    this.showToast('Invalid file.');
-                    return;
-                }
-
-                this.levels = roulette.levels;
-                this.progression = roulette.progression;
-                this.save();
-                this.givenUp = false;
-                this.showRemaining = false;
-                this.percentage = undefined;
-            } catch {
-                this.showToast('Invalid file.');
-                return;
-            }
-        },
-        onExport() {
-            const file = new Blob(
-                [JSON.stringify({
-                    levels: this.levels,
-                    progression: this.progression,
-                })],
-                { type: 'application/json' },
-            );
-            const a = document.createElement('a');
-            a.href = URL.createObjectURL(file);
-            a.download = 'tsl_roulette';
-            a.click();
-            URL.revokeObjectURL(a.href);
-        },
-        showToast(msg) {
-            this.toasts.push(msg);
-            setTimeout(() => {
-                this.toasts.shift();
-            }, 3000);
+            this.loading = false;
         },
     },
 };
